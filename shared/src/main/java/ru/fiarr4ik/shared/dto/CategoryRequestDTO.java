78093ddc8dbe0d9d613b177cae26998badad9a75
Fiarr4ik <fiarr4ik@icloud.com>
2025-08-30 22:45:20 +0700

    feat(category-service): - Сделал сущность категории запчастей. feat(shared): - Добавил нужные зависимости(Ломбок и валидация). - Сделал базовые дто для сущности Category

---
+++ shared/src/main/java/ru/fiarr4ik/shared/dto/CategoryRequestDTO.java
@@ -0,0 +1,21 @@
+package ru.fiarr4ik.shared.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRequestDTO {
+
+    @NotBlank(message = "Название категории не должно быть пустым")
+    @Size(max = 50, message = "Название категории не может быть длиннее 50 символов")
+    private String CategoryName;
+
+    @Size(max = 200, message = "Описание не может быть длиннее 200 символов")
+    private String CategoryDescription;
+}
---
+++ shared/src/main/java/ru/fiarr4ik/shared/dto/CategoryResponseDTO.java
@@ -0,0 +1,10 @@
+package ru.fiarr4ik.shared.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryResponseDTO {
+    private long CategoryId;
+    private String CategoryName;
+    private String CategoryDescription;
+}
