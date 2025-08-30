9b154675c6934a1260ce7ec77e5675a89ea197c0
Fiarr4ik <fiarr4ik@icloud.com>
2025-08-30 12:08:46 +0700

    feat(category-service): - Сделал новый сервис для категорий запчастей. fix(parent): - Исправил pom чтобы зависимости подтягивались. fix(shared): - Добавил packaging, чтобы он стал модулем библиотекой.

---
+++ category-service/src/main/java/ru/fiarr4ik/category_service/CategoryService.java
@@ -0,0 +1,11 @@
+package ru.fiarr4ik.category_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryService {
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryService.class, args);
+    }
+}
